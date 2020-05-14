82c8c7b3cbda9a083cc09178a12e87de4c4fc90d
NFilippa <nfilippa.amulen.unrc@gmail.com>
2020-05-14 15:07:06 -0300

    Se crean archivos en la base de datos. Visibility true

--- app.rb
+++ app.rb
@@ -47,7 +47,7 @@ class App < Sinatra::Base
     request.body.rewind
     hash = Rack::Utils.parse_nested_query(request.body.read)
     params = JSON.parse hash.to_json 
-    #document = Document.new(title: params["title"], type: params["type"], format: ["format"])#format: params["format"])
+    document = Document.new(title: params["title"], type: params["type"], format: ["format"], visibility: true)#format: params["format"])
     
     if document.title && document.title != "" && document.type && document.format 
       document.save
