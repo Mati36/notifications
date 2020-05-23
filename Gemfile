4259899a2ac9ef280560962787a59109065b81f8
FakuuFernandez <fakuufernandez@hotmail.com>
2020-05-22 21:53:16 -0300

    Creacion de test de usuario

--- Gemfile
+++ Gemfile
@@ -5,10 +5,15 @@ gem 'puma', '4.3.3'
 gem 'sinatra', '2.0.7'
 gem 'sequel'
 gem 'pg'
-gem 'bootstrap', '~> 4.4.1'
+#gem 'bootstrap', '~> 4.4.1'
 
 group :development do
   gem 'dotenv', '2.7.5'
   gem 'rerun'
 end
 
+group :test do
+  gem 'minitest'
+  gem 'rack-test'
+  gem 'sqlite3'
+end
---
+++ tests/models/user_test.rb
@@ -0,0 +1,56 @@
+require File.expand_path '../../test_helper.rb', __FILE__
+
+class UserTest < MiniTest::Unit::TestCase
+
+  MiniTest::Unit::TestCase    
+  def test_name_presence
+    @user = User.create(name: 'Facundo', lastname: 'Fernandez', dni: 41593885, email: 'ff@gmail.com', password: '123')
+    assert_equal @user.valid?, true
+  end  
+
+  def test_name_presence
+    @user = User.new(name: '', lastname: 'Fernandez', dni: 41593885, email: 'ff@gmail.com', password: '123')
+    assert_equal @user.valid?, false
+  end
+
+  def test_name_presence
+    @user = User.new(lastname: 'Fernandez', dni: 41593885, email: 'ff@gmail.com', password: '123')
+    assert_equal @user.valid?, false
+  end
+
+  def test_lastname_presence
+    @user = User.new(name: 'Facundo', lastname: '', dni: 41593885, email: 'ff@gmail.com', password: '123')
+    assert_equal @user.valid?, false
+  end
+
+  def test_lastname_presence
+    @user = User.new(name: 'Facundo', dni: 41593885, email: 'ff@gmail.com', password: '123')
+    assert_equal @user.valid?, false
+  end
+
+  def test_dni_presence
+    @user = User.new(name: 'Facundo', lastname: 'Fernandez', dni: '', email: 'ff@gmail.com', password: '123')
+    assert_equal @user.valid?, false
+  end
+
+  def test_dni_presence
+    @user = User.new(name: 'Facundo', lastname: 'Fernandez', email: 'ff@gmail.com', password: '123')
+    assert_equal @user.valid?, false
+  end
+
+  def test_email_presence
+    @user = User.new(name: 'Facundo', lastname: 'Fernandez', dni: 41593885, email: '', password: '123')
+    assert_equal @user.valid?, false
+  end
+
+  def test_email_presence
+    @user = User.new(name: 'Facundo', lastname: 'Fernandez', dni: 41593885, password: '123')
+    assert_equal @user.valid?, false
+  end
+  
+  def test_dni_type
+    @user = User.new(name: 'Facundo', lastname: 'Fernandez', dni: "abc", email: 'ff@gmail.com', password: '123')
+    assert_equal (@user.dni.is_a? Numeric), false
+  end  
+
+end 
