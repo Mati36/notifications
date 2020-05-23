5686ea023df40ac37dc637f6daa175d41f56c2c8
FakuuFernandez <fakuufernandez@hotmail.com>
2020-05-22 23:40:20 -0300

    Creacion de test de documento

---
+++ tests/models/documents_test.rb
@@ -0,0 +1,71 @@
+#:title, :type, :format, :visibility, :path, :created_at
+require File.expand_path '../../test_helper.rb', __FILE__
+
+class UserTest < MiniTest::Unit::TestCase
+
+  MiniTest::Unit::TestCase    
+  def test_title_presence
+    @doc = Document.create(title: 'NuevoDoc', type: 'Act', format: '.pdf', visibility:  true, path: '/files/1.jpeg', created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, true
+  end  
+
+  def test_title_presence
+    @doc = Document.new(title: '', type: 'Act', format: '.pdf', visibility:  true, path: '/files/1.jpeg', created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, false
+  end
+
+  def test_title_presence
+    @doc = Document.new(type: 'Act', format: '.pdf', visibility:  true, path: '/files/1.jpeg', created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, false
+  end
+
+  def test_type_presence
+    @doc = Document.new(title: 'NuevoDoc', type: '', format: '.pdf', visibility:  true, path: '/files/1.jpeg', created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, false
+  end
+
+  def test_type_presence
+    @doc = Document.new(title: 'NuevoDoc', type: 'act', format: '', visibility:  true, path: '/files/1.jpeg', created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, false
+  end
+
+  def test_format_presence
+    @doc = Document.new(title: 'NuevoDoc', type: 'act', format: '', visibility:  true, path: '/files/1.jpeg', created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, false
+  end
+
+  def test_format_presence
+    @doc = Document.new(title: 'NuevoDoc', type: 'act', visibility:  true, path: '/files/1.jpeg', created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, false
+  end
+
+  def test_visibility_presence
+    @doc = Document.new(title: 'NuevoDoc', type: 'act', format: '.pdf', visibility: '', path: '/files/1.jpeg', created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, false
+  end
+
+  def test_visibility_presence
+    @doc = Document.new(title: 'NuevoDoc', type: 'act', format: '.pdf', path: '/files/1.jpeg', created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, false
+  end
+
+  def test_path_presence
+    @doc = Document.new(title: 'NuevoDoc', type: 'act', format: '.pdf', visibility: true, path: '', created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, false
+  end
+
+  def test_path_presence
+    @doc = Document.new(title: 'NuevoDoc', type: 'act', format: '.pdf', visibility: true, created_at: '2020-05-23 03:29:15')
+    assert_equal @user.valid?, false
+  end
+
+  def test_date_presence
+    @doc = Document.new(title: 'NuevoDoc', type: 'act', format: '.pdf', visibility: true, path: '/files/1.jpeg', created_at: '')
+    assert_equal @user.valid?, false
+  end
+
+  def test_date_presence
+    @doc = Document.new(title: 'NuevoDoc', type: 'act', format: '.pdf', visibility: true, path: '/files/1.jpeg')
+    assert_equal @user.valid?, false
+  end
+end  
